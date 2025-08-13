139cfb9b73651d577c71fb8522ed2fed53ed702b
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-08-13 17:48:17 -0300

    Primeiro código em Python

---
+++ codigo1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o numero 1: "))
+num2 = int(input("Digite o numero 2: "))
+
+if num1 > num2:
+    outro_num = num1
+else:
+    outro_num = num2
+    
+print("O maior numero é: ", outro_num)
